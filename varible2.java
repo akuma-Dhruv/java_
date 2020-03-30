1082700a646340ec796f9d6275e899146242e6b3
Dhruv Garg <60261904+akuma-Dhruv@users.noreply.github.com>
2020-03-30 20:37:46 +0530

    getting familer with varibles

---
+++ varible2.java
@@ -0,0 +1,22 @@
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    String fname,lname;
+	    fname="Dhruv ";
+	    lname="Garg";
+	    int len;
+	    len=fname.length()+lname.length();
+	    
+	    double id =75.643;
+	    boolean mode =false;
+	    
+	    
+	    System.out.println(fname+lname);		
+	    System.out.println(len++);		
+	    System.out.println(id+"\n");
+		System.out.println(mode);
+	}
+}
