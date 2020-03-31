367f378bf3f478f7081142929abd17729cc89bde
Dhruv Garg <60261904+akuma-Dhruv@users.noreply.github.com>
2020-03-31 13:07:35 +0530

    if elseif

---
+++ if else.java
@@ -0,0 +1,18 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int arr[]={4,5};
+		if(arr[0]>arr[1])
+		{
+		    System.out.println(arr[0]);
+		    
+		}
+		else if(arr[1]>arr[0])
+		{
+		    System.out.println(arr[1]);
+		}
+		else
+		System.out.println("both equal");
+	}	
+}
