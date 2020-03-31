5d6b1622ebfa5dd0d9376b6db0cdd3d5864a20b0
Dhruv Garg <60261904+akuma-Dhruv@users.noreply.github.com>
2020-03-31 19:17:08 +0530

    triangularNumber

---
+++ triangularNumber.java
@@ -0,0 +1,15 @@
+public class Main
+{
+	public static void main(String[] args) {
+	
+	int tn=0;
+	for (int i=1;i<11 ;i++ ) 
+	{
+
+	 tn= i*(i+1)/2; 
+	System.out.println(tn);
+	    
+	    
+	}
+	}
+}
