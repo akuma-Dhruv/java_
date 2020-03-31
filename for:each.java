0df77b0d6bb00dc246f78091454b494b6545a240
Dhruv Garg <60261904+akuma-Dhruv@users.noreply.github.com>
2020-03-31 19:31:39 +0530

    for :each loop

---
+++ for:each.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+	
+    	String names[] = {"Dhruv", "Tom","Tanisha","Robin"};
+	    for (String fname : names )
+	    
+	{
+	    System.out.println(fname);
+	    
+	    
+	}
+	}
+}
