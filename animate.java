7b2e7342cd23d547953fbf62fc1ebcf647e124d2
Dhruv Garg <60261904+akuma-Dhruv@users.noreply.github.com>
2020-04-01 12:50:15 +0530

    switch image
    android studio code

--- animate.java
+++ animate.java
@@ -1,6 +1,6 @@
 // code to switch between two given images by anmiating
 //change variable and image id accordingly
-
+// full project along with screenshots  https://github.com/akuma-Dhruv/android/tree/master/Animation
 
     boolean ontap=true;
     public void bartTap(View view)
