91c239ce53d693ee368c5f485ee851845e80be56
Dhruv Garg <60261904+akuma-Dhruv@users.noreply.github.com>
2020-04-02 13:48:27 +0530

    android java code for sound

---
+++ soundfile.java
@@ -0,0 +1,5 @@
+MediaPlayer mediaPlayer;//(initilising variable)
+        mediaPlayer = MediaPlayer.create(this,R.raw.(fileName);//intilising life
+
+        mediaPlayer.start();//start playing sound
+        mediaPlayer.setLooping(true);//to make soound play in loop
