b1b01d06440202a888bde0f4066d93e99806dc99
Dhruv Garg <60261904+akuma-Dhruv@users.noreply.github.com>
2020-04-03 19:42:30 +0530

    seekbar as volume controler

---
+++ seekbar.java
@@ -0,0 +1,34 @@
+//here a seekbar is initilize as volume controler
+//change code accordingly
+import android.widget.SeekBar;
+import android.media.AudioManager;
+//will be imported automatically 
+
+
+AudioManager audioManager;
+
+audioManager= (AudioManager) getSystemService(AUDIO_SERVICE);
+        sound =MediaPlayer.create(this,R.raw.sony);
+
+        int maxvVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        SeekBar volumeControl;
+        volumeControl = (SeekBar) findViewById(R.id.seekBar);
+        volumeControl.setMax(maxvVolume);
+
+        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,progress,0);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
