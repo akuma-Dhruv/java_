210be535653aff6361bf12dba611ebbd278f37dd
Dhruv Garg <60261904+akuma-Dhruv@users.noreply.github.com>
2020-04-08 17:57:54 +0530

    download image

--- imageDownload.java
+++ imageDownload.java
@@ -18,3 +18,16 @@
                 return null;
 
             }
+         
+         /////////////////////////for button 
+         ImageView imageView;
+        public void downloadImage(View view)
+        { ImageDownloader task = new ImageDownloader();
+            Bitmap myimage;
+            try {
+                myimage= task.execute("https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.ideacustom.com%2Fsites%2Fdefault%2Ffiles%2Fshowcase-original-images%2FBird-Eyes_Vector_0.jpg&f=1&nofb=1").get();
+                imageView.setImageBitmap(myimage);
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+
